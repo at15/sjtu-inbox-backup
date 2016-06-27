943f1450a57aadf5546cbc6debfd5723d02b800e
unknown <at15@outlook.com>
2016-06-27 22:42:44 +0800

    Fetch message and get raw data

--- play_imapclient.py
+++ play_imapclient.py
@@ -6,6 +6,7 @@ from imapclient import IMAPClient
 with open('config.json', 'r') as configFile:
     config = json.loads(configFile.read())
 
+# Login
 HOST = 'mail.sjtu.edu.cn'
 USERNAME = config['name']
 PASSWORD = config['password']
@@ -15,6 +16,7 @@ ssl = False
 server = IMAPClient(HOST, use_uid=True, ssl=ssl)
 server.login(USERNAME, PASSWORD)
 
+# List all the folders
 folders = server.list_folders()
 # print(folders)
 for folder in folders:
@@ -25,5 +27,25 @@ for k in inbox:
     print(k)
     print(inbox[k])
 
-# FIXME: why no 'EXISTS'
-print(inbox[b'EXISTS'])
+# FIXME: why not 'EXISTS'
+print("%d messages in INBOX folder " % inbox[b'EXISTS'])
+
+messages = server.search(['NOT', 'DELETED'])
+print("%d messages that aren't deleted" % len(messages))
+
+# messages is an array of message_id
+print(type(messages[0]))  # <class 'int'>
+for message in messages:
+    print(message)
+
+# get the first message
+first_message = messages[0]
+# TODO: how to fetch the parsed body, contact and attachments
+response = server.fetch([first_message], ['RFC822'])
+
+# NOTE: the example in doc use iteritems() while py3 use items()
+for message_id, data in response.items():
+    print(message_id)
+    print(data)
+
+print(server.logout())
